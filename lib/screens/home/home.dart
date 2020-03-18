21a2d748264248e80ffdc035c8b4cd44c24a4d93
peawza <56231020+peawza@users.noreply.github.com>
2020-03-18 22:18:56 +0700

    123

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,14 +1,19 @@
+import 'dart:io';
+
 import 'package:projectflutterbeta/models/brew.dart';
+import 'package:projectflutterbeta/screens/home/appbar.dart';
 import 'package:projectflutterbeta/screens/home/brew_list.dart';
+import 'package:projectflutterbeta/screens/home/pang/home.dart';
 import 'package:projectflutterbeta/screens/home/settings_form.dart';
 import 'package:projectflutterbeta/services/auth.dart';
 import 'package:projectflutterbeta/services/database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+final AuthService _auth = AuthService();
+
 
 class Home extends StatelessWidget {
-  final AuthService _auth = AuthService();
-
   @override
   Widget build(BuildContext context) {
     void _showSettingsPanel() {
@@ -22,11 +27,95 @@ class Home extends StatelessWidget {
           });
     }
 
+
+    Widget Mydrawertab1 =Drawer(
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: <Color>[
+                    Colors.red,
+                    Colors.white,
+                    Colors.blue,
+                    Colors.white,
+                    Colors.red,
+                  ])
+              ),
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Material(
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                      elevation: 10,
+                      child: Padding(padding: EdgeInsets.all(8.0),
+                          child :Image.asset('assets/immages/rsm2.jpg',width: 100,height: 100,)),
+                    ),
+                    Text('Name',style: TextStyle(color: Colors.white,fontSize: 15.0),),
+                  ],
+                ),
+              )),
+          CustomListTile(Icons.person,'Profile',()=>{}),
+          CustomListTile(Icons.edit,'Changed-Password',()=>{}),
+          CustomListTile(Icons.settings,'Setting',()=>{}),
+          //
+
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+            child: Container(
+              decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(color: Colors.grey.shade400))
+              ),
+              child: InkWell(
+                splashColor: Colors.orangeAccent,
+                onTap: () async{
+                  Navigator.pop(context);
+                  {
+                    await _auth.signOut();
+                  }
+
+
+                },
+
+                child: Container(
+                  height: 50,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Icon(Icons.exit_to_app),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('Logout',style: TextStyle(
+                                fontSize: 16.0
+                            ),),
+                          ),
+                        ],
+                      ),
+                      Icon(Icons.arrow_right)
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          //
+          CustomListTile(Icons.exit_to_app,'exitApp',()=>exit(0)),
+        ],
+      ),
+    );
+
+
+
     return StreamProvider<List<Brew>>.value(
       value: DatabaseService().brews,
       child: Scaffold(
         backgroundColor: Colors.brown[50],
-        appBar: AppBar(
+        appBar:appBartab,
+
+        /*
+        AppBar(
           title: Text('Brew Crew'),
           backgroundColor: Colors.brown[400],
           elevation: 0.0,
@@ -44,8 +133,73 @@ class Home extends StatelessWidget {
               onPressed: () => _showSettingsPanel(),
             )
           ],
+        ),*/
+        drawer: Mydrawertab1,
+
+        body: Home_Pang(),
+      ),
+    );
+  }
+}
+
+
+
+Widget appBartab = AppBar(
+  title: Text('Brew Crew'),
+  backgroundColor: Colors.brown[400],
+  elevation: 0.0,
+  actions: <Widget>[
+    FlatButton.icon(
+      icon: Icon(Icons.person),
+      label: Text('logout'),
+      onPressed: () async {
+        await _auth.signOut();
+      },
+    ),
+  ],
+);
+
+
+
+
+class CustomListTile extends StatelessWidget{
+  IconData icon;
+  String text;
+  Function onTap;
+  CustomListTile(this.icon,this.text,this.onTap);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border(bottom: BorderSide(color: Colors.grey.shade400))
         ),
-        body: BrewList(),
+        child: InkWell(
+          splashColor: Colors.orangeAccent,
+          onTap: onTap,
+          child: Container(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Icon(icon),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(text,style: TextStyle(
+                          fontSize: 16.0
+                      ),),
+                    ),
+                  ],
+                ),
+                Icon(Icons.arrow_right)
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
---
+++ lib/screens/home/pang/home.dart
@@ -0,0 +1,161 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:projectflutterbeta/screens/home/brew_title.dart';
+import 'package:projectflutterbeta/screens/home/pang/convertermoney.dart';
+import 'package:projectflutterbeta/screens/home/pang/food.dart';
+import 'package:provider/provider.dart';
+import 'package:projectflutterbeta/models/brew.dart';
+class Home_Pang extends StatefulWidget {
+  @override
+  _Home_PangState createState() => _Home_PangState();
+}
+
+class _Home_PangState extends State<Home_Pang> {
+  @override
+  Widget build(BuildContext context) {
+    return  ListView(
+
+      children: <Widget>[
+        SizedBox(
+          height: 30,
+        ),
+        Center(
+          child: Row(
+            children: <Widget>[
+              SizedBox(
+                width: 25,
+              ),
+              Card(
+                child: Material(
+                  child: ClipOval(
+                    child: Card(
+                      child: Container(
+                        width: 150.0,
+                        height: 150.0,
+
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/immages/coin.png'),
+                          ),
+                        ),
+                        child: FlatButton(
+                            padding: EdgeInsets.all(0.0),
+                            //CurrencyConverter
+                            onPressed: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=>CurrencyConverter()));
+                              //debugPrint('Button clicked');
+                            },
+                            child: null
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 25,
+              ),
+              Card(
+                child: Material(
+                  child: ClipOval(
+                    child: Card(
+                      child: Container(
+                        width: 150.0,
+                        height: 150.0,
+                        //alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/immages/tableware.png'),
+                          ),
+                        ),
+                        child: FlatButton(
+                            padding: EdgeInsets.all(0.0),
+                            onPressed: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=>food_pang()));
+                              //debugPrint('Button clicked');
+                            },
+                            child: null
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        Center(
+          child: Row(
+            children: <Widget>[
+              SizedBox(
+                width: 25,
+              ),
+              Card(
+                child: Material(
+                  child: ClipOval(
+                    child: Card(
+                      child: Container(
+                        width: 150.0,
+                        height: 150.0,
+                        //alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/immages/telephone.png'),
+                          ),
+                        ),
+                        child: FlatButton(
+                            padding: EdgeInsets.all(0.0),
+                            onPressed: (){
+                              //Navigator.push(context, MaterialPageRoute(builder: (context)=>page2()));
+                              //debugPrint('Button clicked');
+                            },
+                            child: null
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 25,
+              ),
+              Card(
+                child: Material(
+                  child: ClipOval(
+                    child: Card(
+                      child: Container(
+                        width: 150.0,
+                        height: 150.0,
+                        //alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/immages/temple.png'),
+                          ),
+                        ),
+                        child: FlatButton(
+                            padding: EdgeInsets.all(0.0),
+                            onPressed: (){
+                              //Navigator.push(context, MaterialPageRoute(builder: (context)=>templex1()));
+                              //debugPrint('Button clicked');
+                            },
+                            child: null
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ],
+    ) ;
+  }
+}
+
+
+
